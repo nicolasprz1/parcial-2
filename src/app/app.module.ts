e2f2f14ca57065e9920c2f99ac485e3fa4ef49e3
Nicolas Perez <n.perezr@uniandes.edu.co>
2024-05-15 10:48:44 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PacienteModule } from './paciente/paciente.module';
-import { PacienteService } from './paciente/paciente.service';
-import { PacienteDetailComponent } from './paciente/paciente-detail/paciente-detail.component';
-import { PacienteListComponent } from './paciente/paciente-list/paciente-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PacienteDetailComponent,
-    PacienteListComponent
+    AppComponent,   
   ],
   imports: [
     BrowserModule,
